bba206f999d0392d4f577ea85d049c449d0179db
Kent Larsson <kent@c8.se>
2025-04-28 10:25:43 +0200

    autocommit complete 1025 (43)

--- DEVOPS24/lektion7/process_substitution/process_substitution.sh
+++ DEVOPS24/lektion7/process_substitution/process_substitution.sh
@@ -3,9 +3,12 @@
 # För kommandon som behöver en fil
 
 # Utan process substitution
-ls -1 kat_a/ > kat_a_filer.txt
-ls -1 kat_b/ > kat_b_filer.txt
+ls -1 kat_a/ >kat_a_filer.txt
+ls -1 kat_b/ >kat_b_filer.txt
 diff kat_a_filer.txt kat_b_filer.txt
 rm kat_a_filer.txt kat_b_filer.txt
 
-#
+# Med process substitution
+diff <(ls -1 kat_a/) <(ls -1 kat_b/)
+# 2d1
+# < 2
