277a4d0a00638685ebe630834f38a3fc5d5e268d
Kent Larsson <kent@c8.se>
2025-04-28 10:45:12 +0200

    autocommit complete 1045 (12)

--- DEVOPS24/lektion7/shell_opts/so1.sh
+++ DEVOPS24/lektion7/shell_opts/so1.sh
@@ -4,6 +4,9 @@ set -e
 
 # -e  Exit immediately if a command exits with a non-zero status.
 
+# Vi sväljer felet
 test -f finnsinte || true
+echo Denna rad körs
 
+test -f finnsinte
 echo Denna rad körs inte
