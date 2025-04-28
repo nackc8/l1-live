a8ce573ead41e1f9ad608b54dd8b28422e841a55
Kent Larsson <kent@c8.se>
2025-04-28 10:35:12 +0200

    autocommit complete 1035 (12)

--- DEVOPS24/lektion7/arit_for/arit_for.sh
+++ DEVOPS24/lektion7/arit_for/arit_for.sh
@@ -8,3 +8,6 @@ for x in bosse eva; do echo "$x"; done
 
 # vanlig for-loop över .png filer
 for x in *.png; do echo "$x"; done
+
+# vanlig for-loop över kort
+for x in {hjarter,spader}{1..10}; do echo "$x"; done
