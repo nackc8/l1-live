57c2da0ebc99802520625ee699accbe4386d5a32
Kent Larsson <kent@c8.se>
2025-04-28 09:37:35 +0200

    autocommit complete 0937 (35)

--- DEVOPS24/lektion7/brace_expansion/brace_expansion.sh
+++ DEVOPS24/lektion7/brace_expansion/brace_expansion.sh
@@ -4,4 +4,7 @@ echo innan_{alfa,beta,hej}_efter
 # innan_alfa_efter innan_beta_efter innan_hej_efter
 
 echo innan_{1..5}_efter
-innan_1_efter innan_2_efter innan_3_efter innan_4_efter innan_5_efter
+# innan_1_efter innan_2_efter innan_3_efter innan_4_efter innan_5_efter
+
+echo innan_{a..z}_efter
+# innan_a_efter innan_b_efter innan_c_efter innan_d_efter innan_e_efter innan_f_efter innan_g_efter
