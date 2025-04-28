bee5bdfd2b607aa92358b37adf85668905896699
Kent Larsson <kent@c8.se>
2025-04-28 12:47:56 +0200

    autocommit complete 1247 (56)

--- DEVOPS24/lektion7/trap_signal/trap3.sh
+++ DEVOPS24/lektion7/trap_signal/trap3.sh
@@ -5,6 +5,10 @@ tempfil="temp$RANDOM"
 # EXIT körs vid Ctrl+C eller skriptets slut
 trap 'rm -v $tempfil;exit 0' EXIT
 
+# Ovan funkar med ändrat värde, $tempfil evalueras
+# när trappen körs.
+tempfil="temp$RANDOM"
+
 for ((val = 0; val < 30; val++)); do
 	echo Snurr "$val" | tee -a "$tempfil"
 	sleep 0
