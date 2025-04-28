a0b0224838d4ed7a28d4a8ed38e71a3e0f8de0ca
Kent Larsson <kent@c8.se>
2025-04-28 12:43:25 +0200

    autocommit complete 1243 (25)

--- DEVOPS24/lektion7/trap_signal/trap2.sh
+++ DEVOPS24/lektion7/trap_signal/trap2.sh
@@ -3,7 +3,7 @@
 # EXIT körs vid Ctrl+C eller skriptets slut
 trap 'rm -v minfil.txt;exit 0' EXIT
 
-for ((val = 0; val < 10; val++)); do
+for ((val = 0; val < 100; val++)); do
 	echo Snurr "$val" | tee -a minfil.txt
 	sleep 1
 done
