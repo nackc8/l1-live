cd0caff3338607608b943959f537f3821165460c
Kent Larsson <kent@c8.se>
2025-04-28 13:14:56 +0200

    autocommit complete 1314 (56)

--- DEVOPS24/lektion7/funk/funk3.sh
+++ DEVOPS24/lektion7/funk/funk3.sh
@@ -2,6 +2,7 @@
 
 # Funktion som körs i barnprocess!
 foo() (
+	echo Process "$BASHPID"
 	umask 123
 	for namn; do
 		echo "Hej $namn"
@@ -10,5 +11,6 @@ foo() (
 	echo Slut på funktionen
 )
 
+echo Process "$BASHPID"
 foo "$@" # Betyder foo "$1" "$2" "$3" ..
 umask
