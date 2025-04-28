cba893763909862ea7d29987c7671f4b934dcea7
Kent Larsson <kent@c8.se>
2025-04-28 12:53:20 +0200

    autocommit partial 1253 (20)

--- DEVOPS24/lektion7/trap_signal/trap4.sh
+++ DEVOPS24/lektion7/trap_signal/trap4.sh
@@ -3,7 +3,7 @@
 # EXIT körs vid Ctrl+C eller skriptets slut
 trap 'rm -v minfil.txt;exit 0' EXIT
 
-for ((val = 0; val < 30; val++)); do
+for true; do
 	echo Snurr "$val" | tee -a minfil.txt
 	sleep 0
 done
