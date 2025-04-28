85ac62a53ad5c321f2f2f068774f8c0ed5c77a0d
Kent Larsson <kent@c8.se>
2025-04-28 10:39:25 +0200

    autocommit complete 1039 (25)

--- DEVOPS24/lektion7/arit_for/arit_for.sh
+++ DEVOPS24/lektion7/arit_for/arit_for.sh
@@ -19,4 +19,6 @@ for x in {hjarter,spader}{1..10}; do echo "$x"; done
 # format: for (( expr1 ; expr2 ; expr3 )) ; do list ; done
 
 # arit loop över jämna nummer lägre än 20
-for (( t=2 ;  ))
+for ((t = 2; t < 20; t = t + 2)); do
+	echo Tal: "$t"
+done
