e9782df091baefee4eef5abb2ea2c13b93dd1f9d
Kent Larsson <kent@c8.se>
2025-04-28 10:50:10 +0200

    autocommit partial 1050 (10)

--- DEVOPS24/lektion7/shell_opts/so1 copy.sh
+++ DEVOPS24/lektion7/shell_opts/so1 copy.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-set -e
-
-# -e  Exit immediately if a command exits with a non-zero status.
-
-# Vi sväljer felet
-# alt 1: test -f finnsinte || true
-# alt 2: test -f finnsinte && true
-# alt 3:
-# if test -f finnsinte; then
-# 	echo Den fanns
-# fi
-while test -f finnsinte; do
-	echo Den fanns
-	sleep 1
-done
-
-# Slutsats: ALLT som använder exit status, gör att exit status
-# som indikerar fel inte avbryter skriptet.
+set -x
 
 echo Denna rad körs
 
